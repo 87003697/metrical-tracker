724660932faa8910c551697d1b9857f329603308
theEricMa <zm2354@columbia.edu>
2023-04-01 06:36:30 +0000

    add_scripts

---
+++ scripts/justin.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=5
+python tracker.py --cfg ./configs/actors/justin.yml
---
+++ scripts/wojtek.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=6
+python tracker.py --cfg ./configs/actors/wojtek.yml
